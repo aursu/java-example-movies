58ac86c1505c9f29b7f0aa26250ccfbe7b895810
Alexander Ursu <alexander.ursu@gmail.com>
2022-11-07 18:57:06 +0100

    Renamed field name

--- src/delegation/movies/FilmType.java
+++ src/delegation/movies/FilmType.java
@@ -1,15 +1,15 @@
 package delegation.movies;
 
 public enum FilmType {
-    CHILDREN(1.0, 0.0), // 1 USD per day, no bonus
-    REGULAR(2.0, 0.4), // 2 USD per day, 40% bonus
-    NEW_FILM(3.0, 0.1); // 3 USD per day, 10% bonus
+    CHILDREN(1.0, 0.0), // 1 USD per day, no bonus (discount)
+    REGULAR(2.0, 0.4), // 2 USD per day, 40% bonus (discount)
+    NEW_FILM(3.0, 0.1); // 3 USD per day, 10% bonus (discount)
 
     private final double dayPrice;
-    private final double dayBonus;
+    private final double bonus;
 
-    FilmType(double dayPrice, double dayBonus) {
-        this.dayBonus = dayBonus;
+    FilmType(double dayPrice, double bonus) {
+        this.bonus = bonus;
         this.dayPrice = dayPrice;
     }
 
@@ -18,6 +18,6 @@ public enum FilmType {
     }
 
     double getBonus(int days) {
-        return days * dayPrice * dayBonus;
+        return days * dayPrice * bonus;
     }
 }
